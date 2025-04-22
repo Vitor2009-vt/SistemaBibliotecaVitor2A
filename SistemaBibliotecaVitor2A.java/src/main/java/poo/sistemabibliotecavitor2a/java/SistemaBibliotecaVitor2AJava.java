fbc441e9b24964fd54a0591d99f4b69a3b3c9017
Vitor2009-vt <vitorgatinho0731@gmail.com>
2025-04-22 12:29:48 -0300

    Add files via upload

---
+++ SistemaBibliotecaVitor2A.java/src/main/java/poo/sistemabibliotecavitor2a/java/SistemaBibliotecaVitor2AJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poo.sistemabibliotecavitor2a.java;
+
+/**
+ *
+ * @author 2024105070045
+ */
+public class SistemaBibliotecaVitor2AJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
